c587a299fd841d5d46e5ea950da8e22e9c23de9c
Zero Dev <zero@Zeros-MacBook-Pro.local>
2023-03-14 15:52:29 -0400

    Changed the whole plugins file again...

---
+++ after/plugin/color.lua
@@ -0,0 +1,16 @@
+require('tokyonight').setup({
+    disable_background = true
+})
+
+function ColorMyPencils(color)
+	color = color or "tokyonight"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyPencils()
+
+vim.cmd("AirlineTheme tomorrow")
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,44 +1,117 @@
 return {
 
-    {
-        'nvim-telescope/telescope.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		dependencies = { {'nvim-lua/plenary.nvim'} }
+	},
 
-    'preservim/nerdcommenter',
-    'tpope/vim-fugitive',
+	{'folke/tokyonight.nvim'},
 
-    'folke/tokyonight.nvim',
-    "nvim-treesitter/nvim-treesitter",
+	{
+		'folke/trouble.nvim',
+		config = function()
+			require('trouble').setup {
+				icons = false,
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			}
+		end
+	},
 
-    'mbbill/undotree',
-    'tpope/vim-fugitive',
-    'tc50cal/vim-terminal',
-    'terryma/vim-multiple-cursors',
-    'lervag/vimtex',
-    'gosukiwi/vim-atom-dark',
 
+	{'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+	'nvim-treesitter/playground',
+	'theprimeagen/harpoon',
+	'theprimeagen/refactoring.nvim',
+	'mbbill/undotree',
+	'tpope/vim-fugitive',
+	'nvim-treesitter/nvim-treesitter-context',
 
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        dependencies = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		dependencies = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},         -- Required
-            {'hrsh7th/cmp-nvim-lsp'},     -- Required
-            {'hrsh7th/cmp-buffer'},       -- Optional
-            {'hrsh7th/cmp-path'},         -- Optional
-            {'saadparwaiz1/cmp_luasnip'}, -- Optional
-            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-nvim-lua'},
 
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},             -- Required
-            {'rafamadriz/friendly-snippets'}, -- Optional
-        }
-    },
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'},
+		}
+	},
+
+	'folke/zen-mode.nvim',
+	'github/copilot.vim',
+	'eandrju/cellular-automaton.nvim',
+	'laytan/cloak.nvim',
+
+	--NOTE: Everything that's above this comment has been made by ThePrimeagen he is a fantastic youtuber here is the link to watch his video
+
+	'vim-airline/vim-airline',
+	'vim-airline/vim-airline-themes',
+	'tpope/vim-commentary',
+	'lervag/vimtex',
+	'terryma/vim-multiple-cursors',
+
+	{
+		'glepnir/lspsaga.nvim',
+		branch = 'main',
+		config = function()
+			require('lspsaga').setup({})
+		end,
+		dependencies = {
+			{'nvim-tree/nvim-web-devicons'},
+			--Please make sure you install markdown and markdown_inline parser
+			{'nvim-treesitter/nvim-treesitter'}
+		}
+	},
+
+	{
+		'jinzhongjia/LspUI.nvim',
+		-- event = 'VimEnter',
+		config=function()
+			require('LspUI').setup()
+		end
+	},
+
+	{
+		'windwp/nvim-autopairs',
+		config = function() require('nvim-autopairs').setup {} end
+	},
+
+	{
+		'goolord/alpha-nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function ()
+			require'alpha'.setup(require'alpha.themes.startify'.config)
+		end
+	},
+
+	{
+		'folke/todo-comments.nvim',
+		dependencies = 'nvim-lua/plenary.nvim',
+	},
+
+	{
+		'Kasama/nvim-custom-diagnostic-highlight',
+		config = function()
+			require('nvim-custom-diagnostic-highlight').setup {}
+		end
+	},
+
+	{
+		'christoomey/vim-tmux-navigator'
+	},
 }
