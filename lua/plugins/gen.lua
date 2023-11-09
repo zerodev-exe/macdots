18bfef6cc103779d39efb50b292363863caf870e
Zero DEv <zerodev.exe@proton.me>
2023-11-09 13:56:11 -0500

    finnaly managed to find the error in the gen.lua file

--- lua/plugins/gen.lua
+++ lua/plugins/gen.lua
@@ -1,3 +1,14 @@
 return {
-  "David-Kunz/gen.nvim"
+  "David-Kunz/gen.nvim",
+  config = function ()
+    require('gen').prompts['Elaborate_Text'] = {
+      prompt = "Elaborate the following text:\n$text",
+      replace = true
+    }
+    require('gen').prompts['Fix_Code'] = {
+      prompt = "Fix the following code. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+      replace = true,
+      extract = "```$filetype\n(.-)```"
+    }
+  end
 }
